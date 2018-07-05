a6b4564d69ef0343aef4a45b707eed29cbd2f5d2
Javi Aibar <jaibar@mundoarti.com>
2018-07-05 21:50:15 +0200

    Repair method of addition Nodes, added functionality

--- src/Node.java
+++ src/Node.java
@@ -1,27 +1,53 @@
-public class Nodo {
+public class Node {
     private char name;
     // Number of possible routes
     private int size = 0;
-    private Nodo[] cruces;
-    
-    public Nodo(char name) {
+    private Cross[] crosses;
+
+    public Node(char name) {
         this.name = name;
+        crosses = new Cross[10];
     }
 
-    // WIP
-    public void add(Nodo hacia, int trafficLight){
-        int i;
-        int compare = 0;
-        // TODO review usage of compare
-        for (i = 0; i < size && cruces[i] != null && compare < 0; i++) {
-            compare = cruces[i].getDestinationName() < hacia.getName();
+    private void add(Node towards, int trafficLight, boolean isFirst) {
+        int i = 0;
+        // As names are chars, comparing them is as simple as subtracting them
+        // to see whether the result is positive or negative
+        // Negative means that we haven't past the alphabetical position
+        // The moment when it gets positive, that is its position
+        int compare = -1;
+
+        while (i < size && crosses[i] != null && compare < 0) {
+            compare = crosses[i].getDestinationName() - towards.getName();
+            i++;
         }
 
-        if (i != size) {
-            cruces[size] = hacia;
-            size++;
-        } else {
-            cruces[i].add(hacia);
+        if (compare > 0) {
+            i--;
+            System.arraycopy(crosses, i, crosses, i + 1, size - i);
+        }
+        crosses[i] = new Cross(this, towards, trafficLight);
+        size++;
+        if (isFirst) {
+            towards.add(this, trafficLight, false);
+        }
+    }
+
+    public void add(Node towards, int trafficLight) {
+        add(towards, trafficLight, true);
+    }
+
+    public Cross[] getCrosses() {
+        return crosses;
+    }
+
+    public Node getNode(int i) {
+        return crosses[i].getDestination();
+    }
+
+    public void showCrosses() {
+        for (int i = 0; i < size; i++) {
+            System.out.println(crosses[i]);
         }
     }
 
@@ -35,5 +61,17 @@ public class Nodo {
 
     public int getSize() {
         return size;
+    }
+
+    @Override
+    public String toString() {
+        String res = "Node " + name + ", connected to: ";
+        for (int i = 0; i < size; i++) {
+            res += crosses[i].getDestinationName();
+            if (i != size - 1) {
+                res += ", ";
+            }
+        }
+        return res;
     }
 }
